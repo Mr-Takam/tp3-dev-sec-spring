11582de57a72793c63302b4bb29ce18d42265d4d
Cyril Takam <127041301+Mr-Takam@users.noreply.github.com>
2026-02-22 11:57:42 +0000

    feat: Generate Spring Boot 3.5 project structure with Web, Security, and JPA

---
+++ src/main/java/fr/efrei/tp3_dev_sec_spring/Tp3DevSecSpringApplication.java
@@ -0,0 +1,13 @@
+package fr.efrei.tp3_dev_sec_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3DevSecSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3DevSecSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/efrei/tp3_dev_sec_spring/Tp3DevSecSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.efrei.tp3_dev_sec_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3DevSecSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
